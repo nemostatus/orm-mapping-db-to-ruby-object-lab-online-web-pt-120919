694c2a287df93bee229cfeefa95da70ebf2388fb
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 18:45:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,7 @@ class Student
   sql = <<-SQL 
   SELECT * FROM students WHERE name = ? LIMIT 1 
   SQL
-  B[:conn].execute(sql, name).map do |row|
+  DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
