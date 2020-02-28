af2af0a84ebf42ed7564f653da75f5de48219bab
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 19:14:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,10 @@ def self.first_student_in_grade_10(x)
   WHERE grade = 10 
   LIMIT 1
   SQL
-
+  DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
+  end 
 
   def save
     sql = <<-SQL
