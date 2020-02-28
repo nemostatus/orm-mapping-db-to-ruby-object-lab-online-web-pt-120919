74656273b895776082210aed4f9682574e18d1b1
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 18:39:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,8 @@ class Student
 
   def self.new_from_db(row)
     new_student = self.new 
-    new_student.id = row[0]
+    new_student.id = row[0] 
+    new_student.name = row(name)
   end
 
   def self.all
