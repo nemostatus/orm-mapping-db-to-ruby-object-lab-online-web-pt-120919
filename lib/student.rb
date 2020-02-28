feb2f0655b5cfa215cb89017f37930bd595ad768
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 18:55:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL 
-    SELECT name FROM students WHERE grade IS NOT 12 
+    SELECT * FROM students WHERE grade IS NOT 12 
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
