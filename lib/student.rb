14f91d79ed7686e5a867c3ab8d171f1b0244e2b1
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 18:48:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,8 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL 
-    SELECT * FROM students WHERE grade < 12
+    SELECT * FROM students WHERE grade < 12 
+    SQL
 
   
 
