006d48e69e63aa0fd5900c8e505b32a7da48da94
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-28 18:57:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,7 @@ class Student
     sql = <<-SQL  
     SELECT * FROM students 
     SQL
- DB[:conn].execute    
+ DB[:conn].execute(sql)    
 def self.find_by_name(name)
   sql = <<-SQL 
   SELECT * FROM students WHERE name = ? LIMIT 1 
